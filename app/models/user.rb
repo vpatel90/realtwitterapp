af22d31e37585265a357fe34e5e3c358a2d98af5
Vivek Patel <vnp229@gmail.com>
2016-04-15 23:48:11 -0500

    removed password presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
-  validates :handle, :email, :password, presence: true
+  validates :handle, :email, presence: true
   validates :handle, :email, uniqueness: { case_sensitive: false }
 
 end
